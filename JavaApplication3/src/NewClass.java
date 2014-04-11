be8be427532fd838eb1eb2d2f0704b2b9395d613
max <max@max-PC>
2014-04-11 10:51:08 +0800

    第二次嘗試加上NewClass.java

---
+++ JavaApplication3/src/NewClass.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author max
+ */
+public class NewClass {
+  
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("你好我是馬克思,這是public class NewClass");
+    }
+    
+}
